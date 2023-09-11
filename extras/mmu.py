210e8e119b32a8342b24f8c8b1093a2672fa85d3
Mike <skatermike21988@yahoo.com>
2023-09-10 22:43:23 -0400

    Update mmu.py

--- extras/mmu.py
+++ extras/mmu.py
@@ -599,7 +599,6 @@ class Mmu:
     def handle_connect(self):
         self._setup_logging()
         self.toolhead = self.printer.lookup_object('toolhead')
-        self._log_info("Printer info:\n%s" % repr(self.printer.lookup_object('spoolman')))
         # See if we have a TMC controller capable of current control for filament collision detection and syncing
         # on gear_stepper and tip forming on extruder
         self.selector_tmc = self.gear_tmc = self.extruder_tmc = None
